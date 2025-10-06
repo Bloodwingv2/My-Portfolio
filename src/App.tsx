46710d65a162f24d790e0049d8017efe68788c7f
Bloodwingv2 <bhandariumesh81@gmail.com>
2025-10-06 21:36:15 +0530

    Simple Scramble animations and more placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
 import './App.css'
+import { gsap } from 'gsap'
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { useEffect } from 'react';
+
+gsap.registerPlugin(ScrambleTextPlugin);
+
 
 function App() {
+  
+  useEffect(() => {
+  // Start animation on next frame for smoother init
+  requestAnimationFrame(() => {
+    const tl = gsap.timeline({
+      id: "text-scramble",
+      ease: "power2.inOut",
+      repeat: -1,
+      repeatDelay: 3,
+      yoyo: true,
+    });
+
+    tl.to("#scramble-name", {
+      scrambleText: {
+        text: "Mirang Bhandari",
+        chars: "カタカナひらがな漢字ア",
+        speed: 1,
+        revealDelay: 0.5,
+        tweenLength: false,
+      },
+      duration: 2.8,
+      ease: "power1.inOut",
+    });
+  });
+}, []);
 
   return (
     <main>
@@ -16,9 +47,21 @@ function App() {
             </ul>
           </div>
           <div>
-            <p className='mt-25 font-bold text-8xl text-Black-300 text-left p-5'>Mirang Bhandari</p>
+            <p id = 'scramble-name' className='mt-25 font-bold text-8xl text-Black-300 text-left p-5'>Mirang Bhandari</p>
             <p className='text-5xl text-shadow-black p-5'>I am a developer lorem opsum</p>
             <p className='text-xl text-right p-4'>Passion blah blah</p>
+          </div>
+        </section>
+        <section className='w-full bg-[#FAF3E0] py-20'>
+          <div>
+            <p className='text-7xl font-semibold ml-4'>About</p>
+            <p className='text-2xl mt-10 ml-4'>i am a god blah blah blah coding is what i like blah blah lorem opsum</p>
+          </div>
+        </section>
+        <section className='w-full bg-zinc-900 py-20'>
+          <div>
+            <p className='text-7xl font-semibold text-amber-50 ml-4'>My work</p>
+            <p className='text-2xl font-semibold text-amber-50 mt-10 ml-4'>Blah blah this is my about blah blah lorem opsum</p>
           </div>
         </section>
       </div>
